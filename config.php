9742c34d8384cdc518c68c249f8edc8bb947ae32
grownthc <32686451+grownthc@users.noreply.github.com>
2018-11-18 09:47:33 -0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+/*     Nanotoken Faucet Script     */
+/*      Provided by: [zelles]      */
+/*  Do not remove these comments.  */
+
+/*     HipCoin Faucet Script     */
+/*      Provided by: [hippiesurplus]      */
+/*  Do not remove these comments.  */
+
+$database_host = '';
+$database_user = '';
+$database_pass = '';
+$database = '';
+
+$nanotokend_rpc_server = "";
+$nanotokend_rpc_user = "";
+$nanotokend_rpc_password = "";
+$nanotokend_rpc_port = "";
+
+$payout_amount = rand(0, 10) / 10;
+
+$nanotokend_RPC = "http://".$nanotokend_rpc_user.":".$nanotokend_rpc_password."@".$nanotokend_rpc_server.":".$nanotokend_rpc_port."/";
+
+
+
+$nanotokend = new jsonRPCClient($nanotokend_RPC);
+?>
